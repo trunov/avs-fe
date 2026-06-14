8ea6a76ce2fc6ce3f91c479043d276b96cc8d944
trunov <k.trunov.k@yandex.ru>
2026-06-14 09:25:06 +0300

    add google tag to layout

--- my-site/app/[locale]/layout.tsx
+++ my-site/app/[locale]/layout.tsx
@@ -1,10 +1,10 @@
 import { NextIntlClientProvider, hasLocale } from "next-intl";
 import { setRequestLocale } from "next-intl/server";
 import { notFound } from "next/navigation";
+import Script from "next/script";
 import { routing } from "@/i18n/routing";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-
 import "../globals.css";
 
 export function generateStaticParams() {
@@ -19,14 +19,11 @@ export default async function LocaleLayout({
   params: Promise<{ locale: string }>;
 }) {
   const { locale } = await params;
-
   if (!hasLocale(routing.locales, locale)) {
     notFound();
   }
-
   // Enable static rendering for this locale
   setRequestLocale(locale);
-
   return (
     <html lang={locale} className="w-full h-full">
       <body className="w-full h-full">
@@ -35,6 +32,20 @@ export default async function LocaleLayout({
           <main>{children}</main>
           <Footer />
         </NextIntlClientProvider>
+
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18236975823"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-18236975823');
+          `}
+        </Script>
       </body>
     </html>
   );
